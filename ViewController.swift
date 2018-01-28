613ec6d41b60d69883882a5b3a6e1b4b545a1925
XinZouS <sanking.saphira@gmail.com>
2018-01-28 17:21:12 -0500

    add pointer

--- ViewController.swift
+++ ViewController.swift
@@ -21,20 +21,45 @@ class ViewController: UIViewController {
         return v
     }()
     
+    let averageLabel: UILabel = {
+        let l = UILabel()
+        l.font = UIFont.systemFont(ofSize: 20)
+        return l
+    }()
+    
+    let countLabel: UILabel = {
+        let l = UILabel()
+        l.font = UIFont.systemFont(ofSize: 20)
+        return l
+    }()
+
     private var engine: AVAudioEngine!      //The Audio Engine
     private var player: AVAudioPlayerNode!  //The Player of the audiofile
     private var file = AVAudioFile()        //Where we're going to store the audio file
     private var timer: Timer?               //Timer to update the meter every 0.1 ms
+    
     private var volumeFloat: Float = 0.0    //Where We're going to store the volume float
+    private var volumeAverage: Float = 0.0
+    private var volumes = [Float](repeating: 0.0, count: 20)
+    private var volumeSum: Float = 0.0
+    private var volumeCurrIdx: Int = 0
+    private var volumePeakValue: Float = 0.0
+    private var volumePeakCount: Int = 0 {
+        didSet{
+            //
+        }
+    }
 
-    
-    
+    let pointer = PointerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupProgressView()
+        setupLabels()
         setupAVAudioEngine()
+        
+        setupPointerView()
     }
     
     
@@ -43,9 +68,18 @@ class ViewController: UIViewController {
         progressView.addConstraints(left: view.leftAnchor, top: view.topAnchor, right: view.rightAnchor, bottom: nil, leftConstent: 20, topConstent: 150, rightConstent: 20, bottomConstent: 0, width: 0, height: 5)
     }
     
+    private func setupLabels() {
+        view.addSubview(averageLabel)
+        averageLabel.addConstraints(left: view.leftAnchor, top: view.topAnchor, right: nil, bottom: nil, leftConstent: 60, topConstent: 200, rightConstent: 0, bottomConstent: 0, width: 0, height: 0)
+        
+        view.addSubview(countLabel)
+        countLabel.addConstraints(left: nil, top: view.topAnchor, right: view.rightAnchor, bottom: nil, leftConstent: nil, topConstent: 200, rightConstent: 60, bottomConstent: 0, width: 0, height: 0)
+    }
+
     private func setupAVAudioEngine() {
-        //init engine and player
+        
         engine = AVAudioEngine()
+        
         let audioSession = AVAudioSession.sharedInstance()
         do {
             try audioSession.setCategory(AVAudioSessionCategoryRecord)
@@ -58,11 +92,12 @@ class ViewController: UIViewController {
         let inputNode = engine.inputNode as AVAudioInputNode
         let recordingFormat = inputNode.outputFormat(forBus: 0)
         inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
-            let dataptrptr = buffer.floatChannelData!
-            let dataptr = dataptrptr.pointee
+            let dataFloatChannel = buffer.floatChannelData!
+            let dataptr = dataFloatChannel.pointee
             let datum = dataptr[Int(buffer.frameLength) - 1]
             
-            self.volumeFloat = fabs(datum)
+            self.volumeFloat = fabs(datum) * 10
+            self.volumePeakValue = max(self.volumePeakValue, self.volumeFloat)
         }
         
         engine.prepare()
@@ -73,26 +108,39 @@ class ViewController: UIViewController {
         }        
         
         //start timer to update the meter
-        timer = Timer.scheduledTimer(timeInterval: 0.1 , target: self, selector: #selector(updateMeter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.05 , target: self, selector: #selector(updateMeter), userInfo: nil, repeats: true)
         
     }
     
-    @objc func updateMeter() {
+    @objc func updateMeter() { // call by timer
+        if volumeFloat > 0.1 {
+            volumePeakCount += (volumeFloat > volumeAverage) ? 1 : 0
+            countLabel.text = "cnt = \(volumePeakCount)"
+        }
+        getAverage()
+
         self.progressView.setProgress(volumeFloat, animated: false)
-        
-//        if progressView.progress > 0.8{//turn red if the volume is LOUD
-//            progressView.tintColor = .red
-//        }else{//else green
-//            progressView.tintColor = UIColor.green
-//        }
-        
+        pointer.rotateTo(volumeFloat)
     }
+    
+    private func getAverage() {
+        let len = volumes.count
+        volumeSum -= volumes[volumeCurrIdx]
+        volumes[volumeCurrIdx] = volumeFloat
+        volumeSum += volumeFloat
+        volumeAverage = volumeSum / Float(volumes.count)
+        averageLabel.text = "avg = \(volumeAverage)"
+        volumeCurrIdx = (volumeCurrIdx + 1) % len
+    }
+    
+    private func setupPointerView() {
+        pointer.addPointerInCenterOf(self.view)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
